6a21eba4f19ed7343d1603b5800e0c130388dc08
saqibmehmoodgit <saqib496@hotmail.com>
2020-10-16 15:36:11 +0500

    first change

--- README.md
+++
@@ -1,1 +0,0 @@
-# Java_Ecommerce_Rest
---
+++ src/main/java/com/ecommerce/EcommercSouqApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercSouqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercSouqApplication.class, args);
+	}
+
+}
